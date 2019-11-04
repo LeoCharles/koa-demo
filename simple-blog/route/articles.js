389134502f8255fc1637b70381c94c425ea464b7
liucong <leo-fe@foxmail.com>
2019-11-04 18:06:26 +0800

    :boom: update

--- simple-blog/route/articles.js
+++ simple-blog/route/articles.js
@@ -3,29 +3,19 @@ const controller = require('../controller/articles')
 
 // 重定向
 router.get('/', async ctx => {
-  ctx.redirect('/articles/all')
-})
-router.get('/articles', async ctx => {
-  ctx.redirect('/articles/all')
+  ctx.redirect('/articles')
 })
 
 // 渲染文章发表页
-router.get('/articles/create', controller.getArticlesCreate)
+router.get('/create', controller.getArticlesCreate)
 
 // 提交文章表单
-router.post('/articles/create', controller.postArticlesCreate)
+router.post('/create', controller.postArticlesCreate)
 
-// 渲染全部文章页
-router.get('/articles/all', controller.getArticles)
-
-// 渲染我的文章页
-router.get('/articles/self', controller.getArticles)
+// 渲染全部文章 / 我的文章页
+router.get('/articles', controller.getArticles)
 
 // 渲染文章详情页
-router.get('/articles/:id', async ctx => {
-  await ctx.render('detail', {
-    session: ctx.session
-  })
-})
+router.get('/articles/:id', controller.getArticleDetail)
 
 module.exports = router
