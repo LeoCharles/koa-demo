50aca0c70eee907717d8ec4d4b3b7d3a5b043367
liucong <leo-fe@foxmail.com>
2019-11-05 15:28:04 +0800

    :boom: update

--- simple-blog/controller/user.js
+++ simple-blog/controller/user.js
@@ -38,7 +38,6 @@ exports.postRegister = async ctx => {
     const upload= await uploadImage(avatar, path)
     // 判断头像是否上传成功
     if(!upload) {
-      console.log('头像上传失败')
       return ctx.body = {
         code: 500,
         msg: '头像上传失败'
@@ -48,14 +47,12 @@ exports.postRegister = async ctx => {
     // 向数据库中插入数据
     const time = dayjs().format('YYYY-MM-DD HH:mm:ss')
     await mysql.insertUser([name, md5(password), imageName, time])
-    console.log('注册成功')
     return ctx.body = {
       code: 200,
       msg: '注册成功'
     }
 
   } catch (err) {
-    console.log('注册失败', err)
     return ctx.body = {
       code: 500,
       msg: '注册失败'
@@ -78,8 +75,14 @@ exports.postLogin = async ctx => {
   try {
     // 查询数据库，判断用户密码是否正确
     const rows = await mysql.findUserByName(name)
-    if(rows.length && rows[0]['name'] === name && rows[0]['password'] === md5(password)) {
-      console.log('登录成功')
+    if (!rows.length) {
+      return ctx.body = {
+        code: 500,
+        msg: '用户不存在'
+      }
+    }
+
+    if(rows[0]['name'] === name && rows[0]['password'] === md5(password)) {
       // 登录成功保存 session 信息
       ctx.session = {
         user:  rows[0]['name'],
@@ -93,11 +96,10 @@ exports.postLogin = async ctx => {
     } else {
       return ctx.body = {
         code: 500,
-        msg: '用户名或密码错误'
+        msg: '密码错误'
       }
     }
   } catch (err) {
-    console.log('登录失败', err)
     return ctx.body = {
       code: 500,
       msg: '登录失败'
